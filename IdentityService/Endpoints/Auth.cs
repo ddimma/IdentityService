9c5612c403944cdf11622fe993841a6a5733a9e4
Dumitru Pavlov <Dumitru.Pavlov@endava.com>
2024-11-11 11:01:01 +0200

    Minor changes

--- IdentityService/Endpoints/Auth.cs
+++ IdentityService/Endpoints/Auth.cs
@@ -24,12 +24,6 @@ namespace IdentityService.Endpoints
                         FirstName = request.FirstName,
                         LastName = request.LastName
                     };
-                    //var existingUserByEmail = await userManager.FindByEmailAsync(user.Email);
-                    //var existngUserByUsername = await userManager.FindByNameAsync(user.UserName);
-                    //if (existingUserByEmail is not null)
-                    //{
-                    //    identityErrorsList.Add(new IdentityError { Code = "Email", Description = "Email is taken."});
-                    //}
 
                     var result = await userManager.CreateAsync(user, request.Password);
 
